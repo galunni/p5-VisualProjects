1e423f04edb2be5f3c9a9499c103c5b8d2e5927c
giovanni <galunni@users.noreply.github.com>
2020-04-14 23:32:23 +0200

    updating user interface

--- labyrinthTiles/sketch.js
+++ labyrinthTiles/sketch.js
@@ -1,7 +1,10 @@
-/* inspired by 10 PRINT book */
+/* 
+// labyrinthTiles - giovanni 2020
+// inspired by 10 PRINT book 
+*/
 
 let spc = 24;   //tile size: value btw. 5 and 200
-let r = 0.5;    // orientation: value btw. 0 and 1
+let r = 0.5;    // rotation: value btw. 0 and 1
 let weight = 1; // strokeWeight value btw. 1 and 10
 let slSpc;      // slider for size
 let slR;        // slider for rotation
@@ -15,59 +18,65 @@ let showFillShapes = false;  // true if shapes should be filled
 let bgColorPicker;       // background colorPicker
 let strokeColorPicker;   // line stroke colorPicker
 let fillColorPicker;     // shapes fill colorPicker
+let saveButton;
 
 function setup() {
-	createCanvas(600, 400);
-	slSpc = createSlider(5, 200, 25, 1);
+	createCanvas(800, 500);
+	slSpc = createSlider(5, 200, 25, 1); // slider for size
 	slSpc.position(10, height + 70);
-	slR = createSlider(0, 1, 0.5, 0.01);
+	slR = createSlider(0, 1, 0.5, 0.01);  // slider for rotation
 	slR.position(200, height + 70);
-	slWeight = createSlider(1, 10, 1, 1);
+	slWeight = createSlider(1, 10, 1, 1);  // slider for strokeWeight
 	slWeight.position(400, height + 70);
 	
-	txtSpc = createDiv("Tiles size: " + spc );
-	txtSpc.position(10, height+50);
-	txtR = createDiv("Orientation: " + r );
-	txtR.position(210, height+50);
-	txtWeight = createDiv("Stroke weight: " + weight );
-	txtWeight.position(410, height+50);
+	txtSpc = createDiv("tiles size: " + spc );
+	txtSpc.position(30, height + 90);
+	txtR = createDiv("orientation: " + r );
+	txtR.position(210, height + 90);
+	txtWeight = createDiv("strokeWeight: " + weight );
+	txtWeight.position(420, height + 90);
 	
 	checkBoxBoundaries = createCheckbox('show tiles boundaries', false);
 	checkBoxBoundaries.changed(checkEventBoundaries);
-
+	
 	checkBoxFilled = createCheckbox('fill shapes', false);
 	checkBoxFilled.changed(checkEventFilled);
 	
-	bgColorPicker = createColorPicker('#ffff');
-	bgColorPicker.position(20, height + 110);
+	bgColorPicker = createColorPicker('#0554F2'); // background picker
+	bgColorPicker.position(245, height + 30);
 	bgColorPicker.input( () => renderUpdate = true );
-	strokeColorPicker = createColorPicker(color('#666'));
-	strokeColorPicker.position(220, height + 110);
+	strokeColorPicker = createColorPicker(color('#4CB1F7'));  // stroke picker
+	strokeColorPicker.position(450, height + 30);
 	strokeColorPicker.input( () => renderUpdate = true );
-	fillColorPicker = createColorPicker(color('#666'));
-	fillColorPicker.position(420, height + 110);
-	fillColorPicker.input( () => {if(showFillShapes) renderUpdate = true; } );
+	fillColorPicker = createColorPicker(color('#9af')); // fill picker
+	fillColorPicker.position(100, height + 40);
+	fillColorPicker.input( () => {if(showFillShapes) renderUpdate = true;} );
+	
+	saveButton = createButton('Save Image');
+	saveButton.position(600, height + 50);
+	saveButton.mousePressed( () => { save(); });
+	
 	frameRate(5);
 }
 
 function draw() {
 	if (spc !== slSpc.value() || r !== slR.value() || 
-	    weight !== slWeight.value() || renderUpdate) {
+	weight !== slWeight.value() || renderUpdate) {
 		renderUpdate = false;
 		spc = slSpc.value();
 		r = slR.value();
 		weight = slWeight.value();
-		txtSpc.html("Tiles size: " + spc );
-		txtR.html("Orientation: " + r );
+		txtSpc.html("tiles size: " + spc );
+		txtR.html("orientation: " + r );
 		txtWeight.html("strokeWeight: " + weight );
 		
 		strokeWeight(slWeight.value());
 		stroke(strokeColorPicker.color());
 		background(bgColorPicker.color());
-
+		
 		if(showFillShapes) fill(fillColorPicker.color());
 		else noFill();
-
+		
 		for (var x = 0; x < width; x += spc) {
 			for (var y = 0; y < height; y += spc) {
 				if (random() < r) {
@@ -80,13 +89,14 @@ function draw() {
 					arc(x + spc/2, y + spc/2, spc, spc, 0, HALF_PI);
 				}
 				if(showTilesBoundaries){ 
-				    noFill();
+					noFill();
 					rect(x,y,spc,spc);
 					if(showFillShapes) fill(fillColorPicker.color());
 				}
 			}
 		}
 	}
+	//save();
 }
 
 function checkEventBoundaries(){
